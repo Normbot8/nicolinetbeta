92cf5204fa1f6d9a2e1795fc1a4fe430aa29d726
Normbot8 <normbot8@gmail.com>
2018-03-06 20:03:55 -0500

    NEW RELEASE ALMOST LIVE
    kinda almost ready

--- javascript/location.js
+++
@@ -1,4 +0,0 @@
-function locateLocation(response) {
-    $("#address").html("Thank you for visiting from " + response.city + ", " + response.region_name + "!");
-    $('#latlong').html("For your information (and also to freak you out further): Yer Latitude: " + response.latitude + " Yer Longitude: " + response.longitude);
-};
